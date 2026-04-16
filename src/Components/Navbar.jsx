1ca0ff56671add8e6e246585542013c184bcc2f2
ahsan ali <ahsanxali0987@gmail.com>
2026-04-16 17:24:08 +0500

    abc

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="">
+        <img src="" alt="" />
+      </div>
+      <div className="">
+        <Link to="">
+          <p className=""></p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+
+const HomePage = () => {
+  return (
+    <div className="bg-white">
+      <Navbar />
+    </div>
+  );
+};
+
+export default HomePage;
